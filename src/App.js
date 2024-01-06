f3f5a1ee167c52ac96c7d7fec2cbf88d4d13983d
minhtien213 <minhtien213@gmail.com>
2024-01-06 23:07:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+
+const cars = [
+    {
+        id: 1,
+        name: 'Bugatti',
+    },
+    {
+        id: 2,
+        name: 'Lamborghini',
+    },
+    {
+        id: 3,
+        name: 'Maybach',
+    },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [checked, setChecked] = useState(); //dùng biến checked để lưu id tùng thẻ radio
+        function handlsubmit(){
+            //call API
+            //checked đã chứa giá trị id của radio được checked
+            console.log({id: checked})    
+        }
+    return (
+        <div className="App">
+            {cars.map(car => (
+                <div key={car.id}>
+                    <input type="radio"
+                    //trạng thái check hay không check của radio
+                    checked = {checked === car.id} //nếu giá trị checked(id) === id của car thì true(checked)
+                    //set lại checked(set lại giá trị id) khi radio thay đổi
+                    onChange={() => setChecked(car.id)} /> 
+                    {car.name}
+                </div>
+            ))}
+            <button onClick={handlsubmit}>Submit</button>
+        </div>
+    )
 }
+
 
 export default App;
